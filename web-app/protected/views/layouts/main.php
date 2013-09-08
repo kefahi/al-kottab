7720524cc127e387a0ba1192c143578b7a2b1838
Ameen Sarsour <amas89@gmail.com>
2013-09-08 11:29:25 +0300

    Add Yii files and SechaExtJs

---
+++ web-app/protected/views/layouts/main.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<!-- Auto Generated with Sencha Architect -->
+<!-- Modifications to this file will be overwritten. -->
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>school</title>
+    <script src="/extjs/ext-all-rtl-debug.js"></script>
+
+<!--    <link rel="stylesheet" href="/extjs/resources/ext-theme-gray/ext-theme-gray-all-debug.css">   -->
+    <link rel="stylesheet" href="/extjs/resources/ext-theme-classic/ext-theme-classic-all-debug.css">  
+
+    <script type="text/javascript" src="app.js"></script>
+</head>
+<body></body>
+</html>
